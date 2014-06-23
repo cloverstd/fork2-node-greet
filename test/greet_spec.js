115194a01163e4cc964063603a50b232c4dcf849
cloverstd <cloverstd@gmail.com>
2014-06-24 00:27:29 +0800

    Warm 2

---
+++ test/greet_spec.js
@@ -0,0 +1,16 @@
+var greet = require('../index.js');
+//var expect = require('chai').expect;
+
+var name = 'cloverstd';
+var drunk = true;
+
+describe('greet', function() {
+  it('should greet a person by name', function() {
+    var result = greet(name);
+    expect(result).is.eql('hello ' + name);
+  });
+  it('should greet a person flirtatiously if drunk', function() {
+    var result = greet(name, drunk);
+    expect(result).is.eql('hello ' + name + ', you look sexy today');
+  });
+});
