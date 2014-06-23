8534e062db32092ca4357239127591b0d8e4f9d9
cloverstd <cloverstd@gmail.com>
2014-06-23 12:24:27 +0800

    多参数处理

--- bin/greet.js
+++ bin/greet.js
@@ -4,6 +4,6 @@ var greet = require('greet');
 var parseArgs = require('minimist');
 
 var argv = parseArgs(process.argv.slice(2));
-var result = greet(argv._[0], argv.drunk);
+var result = greet(argv._, argv.drunk);
 
 console.log(result);
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function greet(name, drunk) {
+  var result = 'hello ';
+  if (name.length === 0) {
+    result += 'world';
+  } else {
+    result += name.constructor == Array ? name.join(', ') : name;
+  }
+
   if (drunk)
-    return 'hello ' + name + ', you look sexy today';
+    return result + ', you look sexy today';
   else
-    return 'hello, ' + name;
+    return result;
 }
 
 module.exports = greet;
